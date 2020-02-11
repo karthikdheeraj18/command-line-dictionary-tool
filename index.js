140b3f9820b22a9449d576595b8d22f1e3c7b5a8
Karthik Dheeraj <karthikmy9@gmail.com>
2020-02-11 19:02:33 +0530

    data persistence

--- index.js
+++ index.js
@@ -3,10 +3,15 @@
 require('dotenv').config();
 const fetch = require('node-fetch')
 
+var MongoClient = require('mongodb').MongoClient;
+
 const api_host = process.env.API_HOST
 const api_key = process.env.API_KEY
+const mongodb_connect = process.env.MONGODB_CONNECT
 const defn_type = process.argv[2] || null;
 const word = process.argv[3] || null;
+
+const dictionary_store = []
 
 //console.log(defn_type,word)
 
@@ -125,10 +130,62 @@ function CheckRandomAPI(api_host,api_key) {
     .catch((error) => { console.log(error) })
 }
 
+async function GetAllWordsAPI(api_host,api_key) {
+
+    const allWords = []
+    while (allWords.length < 42){
+        const response = await fetch(`${api_host}/words/randomWord?api_key=${api_key}`)
+        const result = await response.json()
+        console.log(result)
+        if (!allWords.includes(result.word))
+            allWords.push(result.word)
+        console.log(allWords.length)
+    }
+    //console.log(allWords)
+
+    const MongoClient = require('mongodb').MongoClient;
+    const uri = mongodb_connect;
+    //mongodb_connect;
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    
+    await client.connect(async err => {
+        if(err) { return console.dir(err); }
+
+        const collection = await client.db("command-line-dictionary-db").collection("dictionary");
+
+        allWords.map(async d => {
+            const response2= await fetch(`${api_host}/word/${d}/definitions?api_key=${api_key}`)
+            const result_defn_data = await response2.json()
+            const response3= await fetch(`${api_host}/word/${d}/examples?api_key=${api_key}`)
+            const result_ex_data = await response3.json()
+            const response4= await fetch(`${api_host}/word/${d}/relatedWords?api_key=${api_key}`)
+            const result_rw_data = await response4.json()
+    
+            let defns = result_defn_data.map(d => d.text)
+            let exs = result_ex_data.examples.map(d => d.text)
+            let syns = []
+            let ants = []
+    
+            if (result_rw_data[0].relationshipType === 'synonym')
+                syns = result_rw_data[0].words
+            if (result_rw_data[0].relationshipType === 'antonym')
+                ants = result_rw_data[0].words
+            
+            obj = {"word":d,"definitions":defns,"synonyms":syns,"antonyms":ants,"examples":exs}
+            dictionary_store.push(obj)
+
+            await collection.insertOne(obj, {w:1});
+            //console.log(dictionary_store.length)
+        })   
+    });
+    client.close();
+}
+
 function ResolveDefinitionTypes(defn_type, word, api_host, api_key) {
     const types = ["defn", "syn", "ant", "ex", "play"];
-    //var n = fruits.includes("Mango");
 
+    //GetAllWordsAPI(api_host,api_key)
+    
     if (word != null){
         if (defn_type === "defn")
             CheckDefnAPI(word,api_host,api_key);
@@ -145,7 +202,7 @@ function ResolveDefinitionTypes(defn_type, word, api_host, api_key) {
         else if(defn_type == null)
             CheckRandomAPI(api_host,api_key)
         else if(!types.includes(defn_type)){
-            CheckDefnAPI(defn_type,api_host,api_key);
+            CheckDefnAPI(defn_type,api_host,api_key)
             CheckSynAPI(defn_type,api_host,api_key);
             CheckAntAPI(defn_type,api_host,api_key);
             CheckExAPI(defn_type,api_host,api_key);
